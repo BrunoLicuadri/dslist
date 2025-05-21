94573786de24062ecd70a0438a3a7cf910e7192d
Bruno Licuadri <blicuadri@gmail.com>
2025-05-21 09:14:09 -0300

    Domain model

---
+++ src/main/java/com/licuadri/dslist/entities/BelongingPK.java
@@ -0,0 +1,64 @@
+package com.licuadri.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPK {
+	
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	private GameList list;
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	private Game game;
+	
+	
+	public BelongingPK() {
+	}
+
+	public BelongingPK(GameList list, Game game) {
+		this.list = list;
+		this.game = game;
+	}
+
+	public GameList getList() {
+		return list;
+	}
+
+	public void setList(GameList list) {
+		this.list = list;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, list);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+	}
+	
+	
+
+}
