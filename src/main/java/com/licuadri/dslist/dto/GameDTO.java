9212e8e0787035dea2e9cedc3cd5f05490e02cb6
Bruno Licuadri <blicuadri@gmail.com>
2025-05-21 09:45:00 -0300

    GET game by id endpoint

---
+++ src/main/java/com/licuadri/dslist/dto/GameDTO.java
@@ -0,0 +1,104 @@
+package com.licuadri.dslist.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.licuadri.dslist.entities.Game;
+
+import jakarta.persistence.Column;
+
+public class GameDTO {
+	
+	private Long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+
+	
+	public GameDTO() {
+	}
+	
+	//Novidad pra mim essa utilidade deo copyProperties, por isso optei por deixar o exemplo
+	//Contudo, para usálo, teremos de te ro getters e tb o setters.
+	//Mesmo sabendo que normalmente em DTOs não usamos o setters, apenas os getters.
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+	
+}
